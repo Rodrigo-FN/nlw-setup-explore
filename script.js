e1c91c228bf13e44d29ddd92c9f0362a2dcfd086
RodrigoFN <107443321+Rodrigo-FN@users.noreply.github.com>
2023-01-20 21:49:17 -0300

    JS finalizado

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08", "01-09"],
-  takePills: ["01-03", "01-04"],
-  journal: ["01-05", "01-01"],
+button.addEventListener("click", add)
+form.addEventListener('change', save)
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem('NLWSetap@habits', JSON.stringify(nlwSetup.data))
+}
+
+
+const data = JSON.parse(localStorage.getItem("NLWSetap@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
+
